c5b6286aa7a22c1ac081c2ce945333f608a85ad8
Vic Wu <madcocomo@gmail.com>
2019-06-09 16:16:23 +0800

    remove unused import

--- src/main/java/mob/code/blackjack/domain/Paiku.java
+++ src/main/java/mob/code/blackjack/domain/Paiku.java
@@ -1,13 +1,14 @@
 package mob.code.blackjack.domain;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.Iterator;
-import java.util.List;
 
 import static java.util.Arrays.asList;
 
 @Component
+@RequestScope
 public class Paiku {
 
     private Iterator<String> paiku = asList("A8", "B8", "C8").iterator();
